027ca657dc32de622c145f14fa3b4cba9ded13f0
Matthew Urban <mru0002@auburn.edu>
2025-02-06 16:26:26 -0600

    created clock label

--- Assignment4/ViewController.swift
+++ Assignment4/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clockLabel: UILabel!
+    
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startClock()
+        
     }
-
+    
+    func startClock() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateClock() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E, dd MMM yyyy HH:mm:ss"
+        clockLabel.text = formatter.string(from: Date())
+        
+        let formatterTwo = DateFormatter()
+        formatterTwo.dateFormat = "a"
+        
+        let amPm = formatterTwo.string(from: Date())
+        
+        
+        
+        
+    }
 
 }
 
